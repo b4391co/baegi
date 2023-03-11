4a7e56483fbbe84176fa4d0622804f0f1fb4b622
b4391co <b4391co@proton.me>
2023-03-11 19:03:43 +0100

    v0.3.0 polybar sound fix awesome n

--- config/polybar/scripts/pulseaudio-simple.sh
+++ config/polybar/scripts/pulseaudio-simple.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 vol=$((`amixer | grep '%' | head --lines=1 | awk -F '[' '{print $2}' | awk -F '%' '{print $1}'`))
+status=`amixer | grep 'off' | awk -F '[' '{print $3}' | awk -F ']' '{print $1}' | sort -ru`
+if [ "$status" != "" ]
+then
+    echo "%{F#ff1500} %{F#ffffff}0%"
+fi
 if [ $vol -eq 0 ]
 then
-    echo " 0%"
+    echo "%{F#2495e7} %{F#ffffff}0%"
 elif [ $vol -le 50 ]
 then
-    echo " $vol"%
+    echo "%{F#2495e7} %{F#ffffff}$vol"%
 elif [ $vol -le 100 ]
 then
-    echo " $vol"%
+    echo "%{F#2495e7} %{F#ffffff}$vol"%
 fi
