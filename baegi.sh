0f6325954daf9d075820b54d1f4dac20661d745b
b4391co <b4391co@proton.me>
2023-11-08 16:33:02 +0100

    Fix

--- baegi.sh
+++ baegi.sh
@@ -93,8 +93,7 @@ noSelectLamp "$app"
 while [ $selec = 0 ]
 do
     logo
-    printf "[ + ] ( 1"; if [ $app = '1' ] ; then printf '*'; fi; printf " ) - Container LAMP ( docker ) \n"
-    printf "[ + ] ( 2"; if [ $app = '2' ] ; then printf '*'; fi; printf " ) - Container Kali Linux ( docker ) \n"
+    printf "[ + ] ( 2"; if [ $app = '1' ] ; then printf '*'; fi; printf " ) - Container Kali Linux ( docker ) \n"
     printf "[ + ] ( X"; if [ $app = 'X' ] ; then printf '*'; fi; printf " ) - close\n"
     echo ""
     printf "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――"
@@ -106,52 +105,6 @@ do
         exit
     fi
     if [ $app = "1" ]
-    then
-        f_existe mattrayner/lamp
-        logo
-        printf "[ + ] Indica un directorio para crear la base  \n"
-        printf "[ + ] pe: '~/Documentos', '/home/user/Documentos' , '.' \n"
-        echo ""
-        printf "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――"
-        echo ""
-        read directorioLAMP
-        cd $directorioLAMP
-        logo
-        printf "[ + ] como se llamara el proyecto, se creara la carpeta web \n"
-        printf "[ + ] pe: 'web1' '" 
-        printf "'N' para trabajar en el anterior directirio' \n"
-        echo ""
-        printf "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――"
-        echo ""
-        read carpetaLAMP
-        logo
-        printf "[ + ] Crear carpeta de mysql \n"
-        printf "[ + ] y/n' \n"
-        echo ""
-        printf "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――"
-        echo ""
-        read yesSQL
-        NombreLamp=$(generarNombre)
-        if [ $carpetaLAMP = "N" ] || [ $carpetaLAMP = "n" ]
-        then
-            carpetaLAMP=$pwd
-        else
-            mkdir $carpetaLAMP
-        fi
-        cd $directirioLAMP $carpetaLAMP
-        if [ "$yesSQL" = "y" ] || [ "$yesSQL" = "Y" ]
-        then
-            docker run -i -t --name $NombreLamp -p "80:80" -p "3306:3306" -v $carpetaLAMP:/app -v $pwd/mysql:/var/lib/mysql mattrayner/lamp:latest
-            docker rm $NombreLamp
-            break
-        elif [ "$yesSQL" = "N" ] || [ "$yesSQL" = "n" ]
-        then
-            docker run -i -t --name $NombreLamp -p "80:80" -p "3306:3306" -v $carpetaLAMP:/app  mattrayner/lamp:latest
-            docker rm $NombreLamp               
-            break
-        fi
-    fi
-    if [ $app = "2" ]
     then
         f_existe jasonchaffee/kali-linux
         docker run -it jasonchaffee/kali-linux:latest zsh
